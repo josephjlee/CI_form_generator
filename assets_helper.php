e5c94fceeff1767f160b7866c95b57c0496e0b3b
45developer <40198195+45developer@users.noreply.github.com>
2018-06-12 12:47:49 +0200

    have added the array that contains the value too fill in the empty html tags

--- assets_helper.php
+++ assets_helper.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * Created by PhpStorm.
@@ -26,15 +25,15 @@ function customizable_login_form_generator($info_array='')
 
 
 <div class="container">
-    <form action="" method="">
+    <form action="'.$info_array['f_f_action'].'" method="'.$info_array['f_f_method'].'">
         <div class="form-group">
-            <label for="">Username:</label>
-            <input type="email" class="form-control" name="" id="" aria-describedby="emailHelp" placeholder="">
+            <label for="'.$info_array['f_f_id1'].'">Username:</label>
+            <input type="email" class="form-control" name="'.$info_array['f_f_name1'].'" id="'.$info_array['f_f_id1'].'" aria-describedby="emailHelp" placeholder="'.$info_array['f_f_placeholder1'].'">
             <small id="emailHelp" class="form-text text-muted">Make sure it\'s a valid email.</small>
         </div>
         <div class="form-group">
-            <label for="">Password:</label>
-            <input type="password" name="" class="form-control" id="" placeholder="">
+            <label for="'.$info_array['s_f_id2'].'">Password:</label>
+            <input type="password" name="'.$info_array['s_f_name2'].'" class="form-control" id="'.$info_array['s_f_id2'].'" placeholder="'.$info_array['s_f_placeholder2'].'">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
